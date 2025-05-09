4ab241c1f1c33cb7751dd541bd8b0a0b806d51e8
AlekMichael123 <alekmichael2345@gmail.com>
2025-05-08 17:20:13 -0700

    2024 day 1 part 1 complete

---
+++ src/solutions2024/day1.js
@@ -0,0 +1,25 @@
+const day1 = input => {
+  const [leftNums, rightNums] = parseData(input);
+  return {
+    part1: part1(leftNums, rightNums),
+    part2: part2(input),
+  };
+};
+
+const parseData = input => 
+  input.split('\n').reduce((acc, line) => {
+    const [a, b] = line.split(' ').map(Number).filter(Boolean);
+    acc[0].push(a);
+    acc[1].push(b);
+    return acc;
+  }, [[], []]);
+
+const part1 = (leftNums, rightNums) => {
+  leftNums.sort((a, b) => a - b);
+  rightNums.sort((a, b) => a - b);
+  return leftNums.map((num, i) => Math.abs(num - rightNums[i])).reduce((acc, num) => acc + num, 0);
+};
+
+const part2 = input => {};
+
+export default day1;
---
+++ src/solutions2024/index.js
@@ -0,0 +1,5 @@
+import day1 from "./day1";
+
+export const solutions = {
+  1: day1,
+};
